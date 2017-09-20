85d194534c79a9b6ec2f7f6e07c11501cf029e21
Joost Bader <joostbader@hotmail.com>
2017-09-20 22:26:06 +0200

    removed not implemented test for build to succeed

--- Src/Data.Tools.Sql.UnitTesting.Tests/Configuration/TestConfigurationTests.cs
+++ Src/Data.Tools.Sql.UnitTesting.Tests/Configuration/TestConfigurationTests.cs
@@ -6,11 +6,6 @@ namespace Data.Tools.UnitTesting.Tests.Configuration
     [TestClass]
     public class TestConfigurationTests
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            throw new Exception("still ?needed");
-            //Data.Tools.UnitTesting.TestSetup.TestConfig.CreateFromConfiguration(null);
-        }
+        
     }
 }
